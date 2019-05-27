fae6f5c40d00b74bdfe53477eb8bac22e818753b
Ali.Mousavi.Kherad <ali.m@xecorp.co>
2019-05-27 16:35:14 +0430

    adding commandline parser

---
+++ ZinTest.DotNetCore.Host/CommandLine/CommandLineParser.cs
@@ -0,0 +1,14 @@
+namespace ZinTest.DotNetCore.Host.CommandLine
+{
+    public class CommandLineParser
+    {
+        public string CommandLine { get; }
+
+        protected CommandLineParser(string commandLine)
+        {
+            
+        }
+        
+        public static 
+    }
+}
